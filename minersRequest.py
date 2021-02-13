236fff439c762c13d2abaf8a4e81abd878aa34a9
aMellin <aidan.mellin@gmail.com>
2021-02-13 14:20:39 -0500

    Formatting a pretty print for the all statistics + functionality for discord bot + individual calls

--- minersRequest.py
+++ minersRequest.py
@@ -7,23 +7,55 @@ import math
 
 ID = "0xA34eB873b4ffB1142948e34Dd50EE232EA15C5d3"
 
-def request(ID):
+def request(ID,call):
     req = requests.get("https://eth.2miners.com/api/accounts/"+ID)
     rJson = req.json()
-    req_overall(rJson)
+    #req_overall(rJson)
     dec_bal = 9
-    print(avg_hash(rJson['hashrate']))
-    print(get_balance(rJson['stats'].get('balance'),dec_bal))
-    print(get_workers(rJson["workers"]))     
+    #print(avg_hash(rJson['hashrate']))
+    #print(get_balance(rJson['stats'].get('balance'),dec_bal))
+    #print(get_workers(rJson["workers"]))
+    
+    called = {
+        'h': avg_hash(rJson['hashrate']), #Get hashrate
+        'b': get_balance(rJson['stats'].get('balance'),dec_bal), #Get balance
+        'w': get_workers(rJson["workers"]),
+        'a': get_all(rJson['hashrate'],rJson['stats'].get('balance'),dec_bal,rJson["workers"])    
+    }
+    print("\n")
+    print(get_all(rJson['hashrate'],rJson['stats'].get('balance'),dec_bal,rJson["workers"]))
+    
+def get_spaces(tmp):
+    return " " * (len(ID) - len(tmp))
+
+def get_all(hr, bal, dec, wkrs):   
+    spacer = ret_str = "| "+ "-"*len(ID) +" |\n"
+    ret_str = spacer
+    
+    calls = [
+        ID,
+        "Current Balance: "+ str(get_balance(bal,dec)),
+        "Avg Hash: "+str(avg_hash(hr)),
+    ]
+    workers = get_workers(wkrs)
+    
+    # for i in calls:
+    #     ret_str += "| "+i+get_spaces(i)+" |\n"+spacer
+        
+    ret_str += "".join(["| "+i+get_spaces(i)+" |\n"+spacer for i in calls])
+    ret_str += "| Workers:" + get_spaces("Workers:")+" |\n" + "".join(["|      "+i+get_spaces("     "+i)+" |\n"+spacer for i in workers])
+    
+    return ret_str
+         
 
 def req_overall(rJson):
     print(rJson.keys())
 
 def get_workers(workers):
-    ret_str = "Workers:\n"
+    ret = []
     for i in workers.keys():
-        ret_str+="\t"+i+":\tOffline = **"+str(workers[i]['offline'])+"**"
-    return ret_str
+        ret.append(i+": Offline = "+str(workers[i]['offline']))
+    return ret
 
 def avg_hash(hr):
     ret_hr = hr * (math.pow(10,-6))
@@ -38,4 +70,4 @@ def minerChart(rJson):
     for i in range(len(minerCharts)):
         if not (minerCharts[i]["workerOnline"] == 0):
             pprint.pprint(minerCharts[i])
-request(ID)
+request(ID,"a")
