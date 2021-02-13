ed28970ee5b632e4b790f8409e5bb261ff04139d
aMellin <aidan.mellin@gmail.com>
2021-02-13 13:04:49 -0500

    Added functionality for getting worker stats, etc

--- minersRequest.py
+++ minersRequest.py
@@ -6,25 +6,24 @@ import pprint
 import math
 
 ID = "0xA34eB873b4ffB1142948e34Dd50EE232EA15C5d3"
-#ID = "0xbe693de86f1dc0abfe9856eee60ca54e0496a1f6" #Big Miner other guy
 
 def request(ID):
     req = requests.get("https://eth.2miners.com/api/accounts/"+ID)
     rJson = req.json()
-    #req_overall(rJson)
-    #minerChart(rJson)
-    
+    req_overall(rJson)
     dec_bal = 9
-    
-    print(rJson["stats"],"\n")
     print(avg_hash(rJson['hashrate']))
     print(get_balance(rJson['stats'].get('balance'),dec_bal))
-     
-    #print(rJson["payments"],"\n")
-    #print(rJson["sumrewards"],"\n")
+    print(get_workers(rJson["workers"]))     
 
 def req_overall(rJson):
     print(rJson.keys())
+
+def get_workers(workers):
+    ret_str = "Workers:\n"
+    for i in workers.keys():
+        ret_str+="\t"+i+":\tOffline = **"+str(workers[i]['offline'])+"**"
+    return ret_str
 
 def avg_hash(hr):
     ret_hr = hr * (math.pow(10,-6))
