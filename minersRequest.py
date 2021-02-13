cb27dfa9c1f6b65ef8f3807b0df3503fd8079b42
aMellin <aidan.mellin@gmail.com>
2021-02-13 11:58:46 -0500

    Changed data return from string manipulation to manipulating the integers with exponents

--- minersRequest.py
+++ minersRequest.py
@@ -3,6 +3,7 @@
 
 import requests
 import pprint
+import math
 
 ID = "0xA34eB873b4ffB1142948e34Dd50EE232EA15C5d3"
 #ID = "0xbe693de86f1dc0abfe9856eee60ca54e0496a1f6" #Big Miner other guy
@@ -13,9 +14,11 @@ def request(ID):
     #req_overall(rJson)
     #minerChart(rJson)
     
+    dec_bal = 9
+    
     print(rJson["stats"],"\n")
     print(avg_hash(rJson['hashrate']))
-    print(get_balance(rJson['stats'].get('balance')))
+    print(get_balance(rJson['stats'].get('balance'),dec_bal))
      
     #print(rJson["payments"],"\n")
     #print(rJson["sumrewards"],"\n")
@@ -24,23 +27,12 @@ def req_overall(rJson):
     print(rJson.keys())
 
 def avg_hash(hr):
-    ret_str = "{}.{}".format(str(hr)[0:2],str(hr)[2::])
-    return ret_str
+    ret_hr = hr * (math.pow(10,-6))
+    return ret_hr
 
-def get_balance(bal):
+def get_balance(bal,dec):
     #Length of returned string can be any size. However, it only counts up to 9 decimal places. 
-    dec_bal = 9
-    bal_str = str(bal) #Str'd int that is balance
-    dec_diff = len(bal_str) - dec_bal
-    if(len(bal_str) >= dec_bal): #Balance is >= 1
-        ret_str = "{}.{}".format(bal_str[0:dec_diff],bal_str[dec_diff::])
-    else:
-        zeros = ""
-        for i in range(abs(dec_diff)):
-            zeros+="0"
-        ret_str = ".{}{}".format(zeros,bal_str)
-        
-    return ret_str
+    return bal*math.pow(10,-dec )
 
 def minerChart(rJson):
     minerCharts = rJson["minerCharts"]
